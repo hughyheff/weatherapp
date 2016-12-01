d7cb13300324f7da333b9117a98cacefebbee867
Hugh Tures <hughtures@gmail.com>
2016-12-01 16:50:33 -0700

    fuckin with some shite

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,18 @@ class WelcomeController < ApplicationController
   
  
 	def index
-      @states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
+  @states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
 
   response = HTTParty.get("http://api.wunderground.com/api/#{Figaro.env.wunderground_api_key}/geolookup/conditions/q/AZ/Phoenix.json")
+
+  @location = response['location']['city']
+  @temp_f = response['current_observation']['temp_f']
+  @temp_c = response['current_observation']['temp_c']
+  @weather_icon = response['current_observation']['icon_url']
+  @weather_words = response['current_observation']['weather']
+  @forecast_link = response['current_observation']['forecast_url']
+  @real_feel_f = response['current_observation']['feelslike_f']
+  @real_feel_c = response['current_observation']['feelslike_c']
 
   if params[:city] != nil
     params[:city].gsub!(" ", "_")
@@ -17,16 +26,6 @@ class WelcomeController < ApplicationController
   else
     response = HTTParty.get("http://api.wunderground.com/api/#{Figaro.env.wunderground_api_key}/geolookup/conditions/q/AZ/Phoenix.json")
   end
-
-
-  @location = response['location']['city']
-  @temp_f = response['current_observation']['temp_f']
-  @temp_c = response['current_observation']['temp_c']
-  @weather_icon = response['current_observation']['icon_url']
-  @weather_words = response['current_observation']['weather']
-  @forecast_link = response['current_observation']['forecast_url']
-  @real_feel_f = response['current_observation']['feelslike_f']
-  @real_feel_c = response['current_observation']['feelslike_c']
 
   if @weather_words == "Clear"
  			@url = ("https://static.pexels.com/photos/205335/pexels-photo-205335.jpeg")
